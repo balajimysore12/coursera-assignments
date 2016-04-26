f361a29d5280ab3455f060ae8507517fa748cc49
balajimysore12 <balaji1994mysore@gmail.com>
2016-04-26 10:33:28 +0530

    Adding module 5 solution

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -102,7 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
        var chosenCategoryShortName = chooseRandomCategory(categories);
-      
+       var randomCategoryShortName = chosenCategoryShortName.short_name;
     
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -116,9 +116,8 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       // 
       var homeHtmlToInsertIntoMainPage = homeHtml;
-      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
-      console.log("chosenCategoryShortName");
-      homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage, "chosenCategoryShortName", chosenCategoryShortName);
+      randomCategoryShortName = "'" + randomCategoryShortName + "'";
+      homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage, "randomCategoryShortName", randomCategoryShortName);
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that. 
